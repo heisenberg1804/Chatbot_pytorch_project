7ac91e14b024aae3b896ecfdf3a6d59593ad225d
heisenberg1804 <sahil.s.mohanty@gmail.com>
2021-05-04 14:01:52 +0530

    Added a basic GUI using tkinter

--- chat.py
+++ chat.py
@@ -27,27 +27,23 @@ model.eval()
 
 bot_name = "Jarvis"
 print("Let's chat! (type 'quit' to exit)")
-while True:
-    # sentence = "do you use credit cards?"
-    sentence = input("You: ")
-    if sentence == "quit":
-        break
 
-    sentence = tokenize(sentence)
-    X = bag_of_words(sentence, all_words)
-    X = X.reshape(1, X.shape[0])
-    X = torch.from_numpy(X).to(device)
+def get_response(msg):
+        sentence = tokenize(msg)
+        X = bag_of_words(sentence, all_words)
+        X = X.reshape(1, X.shape[0])
+        X = torch.from_numpy(X).to(device)
 
-    output = model(X)
-    _, predicted = torch.max(output, dim=1)
+        output = model(X)
+        _, predicted = torch.max(output, dim=1)
 
-    tag = tags[predicted.item()]
+        tag = tags[predicted.item()]
 
-    probs = torch.softmax(output, dim=1)
-    prob = probs[0][predicted.item()]
-    if prob.item() > 0.75:
-        for intent in intents['intents']:
-            if tag == intent["tag"]:
-                print(f"{bot_name}: {random.choice(intent['responses'])}")
-    else:
-        print(f"{bot_name}: I do not understand...")
+        probs = torch.softmax(output, dim=1)
+        prob = probs[0][predicted.item()]
+        if prob.item() > 0.75:
+            for intent in intents['intents']:
+                if tag == intent["tag"]:
+                    return random.choice(intent['responses'])
+
+        return  "I do not understand..."
